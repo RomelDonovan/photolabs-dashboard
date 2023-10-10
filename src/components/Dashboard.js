030821f6530a0541613da7acb0c5655d0fca69c0
RomelDonovan <romeldonovan@live.ca>
2023-10-10 18:26:03 -0400

    Add Panel class component to dasboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
 
 const data = [
   {
@@ -25,16 +26,30 @@ const data = [
     value: "Lukas Souza"
   }
 ];
+
 class Dashboard extends Component {
-  state = { loading: false }
+  state = {
+    loading: false,
+    focused: null
+  }
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
     if (this.state.loading) {
-      return <Loading/>;
+      return <Loading data={data} />;
     }
 
-    return <main className={dashboardClasses} />;
+    const panels = data.map(panel => (
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+      />
+    ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
