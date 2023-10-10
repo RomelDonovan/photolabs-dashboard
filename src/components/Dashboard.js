1f847d8319e683b5e6b4f536a452efaa18b401cc
RomelDonovan <romeldonovan@live.ca>
2023-10-10 19:28:44 -0400

    Change selectPanel func to set value focus or null

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -28,35 +28,54 @@ const data = [
 ];
 
 class Dashboard extends Component {
+  /* Initial state */
   state = {
     loading: false,
     focused: null
   }
-  
+
+  /* Instance Method */
   selectPanel(id) {
-    this.setState({
-     focused: id
-    });
-   }
-  
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+
+  /* Class Property with Arrow Function */
+  // selectPanel = id => {
+  //   this.setState({
+  //     focused: id
+  //   });
+  // };
+
+  /*
+  The following code would not work because it does not use an arrow function, so the binding of this remains dynamic.
+  */
+  /* Class Property with Function expression */
+  //  selectPanel = function(id) {
+  //   this.setState({
+  //    focused: id
+  //   });
+  // };
+
   render() {
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused
     });
-    
+
     if (this.state.loading) {
       return <Loading data={data} />;
     }
-    
-    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id): data).map(panel => (
+
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data).map(panel => (
       <Panel
-      key={panel.id}
-      id={panel.id}
-      label={panel.label}
-      value={panel.value}
+        key={panel.id}
+        label={panel.label}
+        value={panel.value}
+        onSelect={event => this.selectPanel(panel.id)}
       />
-      ));
-      
+    ));
+
     return <main className={dashboardClasses}>{panels}</main>;
   }
 }
